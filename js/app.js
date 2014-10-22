49c71aaf14a2905f8e2c2bfc49dbc8e8bfb325e4
iGuest <nasichj@uw.ed>
2014-10-22 11:25:54 -0700

    some changes

--- js/app.js
+++ js/app.js
@@ -1,6 +1,22 @@
 /* app.js
 * main script file for our little application
 * */
-
+//this does what the css does
 "use strict";
+jQuery(document).ready(function() {
+    jQuery('p a[href!="#top"]').attr('target', '_blank');
+    jQuery('section').hide().fadeIn(1000);
+    //fades out when it opens
+
+    jQuery('nav a, p a[href="#top"]').click(function(eventObject) {
+       // alert('hey you clicked me');
+        console.log(this.hash);
+        var targetElement = jQuery(this.hash);
+        //this element that raise the element- prevent default prevent it from jumping
+        jQuery('html,body').animate({scrollTop: targetElement.offset().top}, 700);
+
+        eventObject.preventDefault();
+
+    });
+});
 
